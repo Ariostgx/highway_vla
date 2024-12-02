307ce8a315d425c9bdf6a3adf767102a2d91b78a
shuhan <shuhan@utexas.edu>
2024-12-01 21:22:06 -0600

    add lane_change_collision spec

--- src/auto_labeling/highway_env/lane_change.py
+++ src/auto_labeling/highway_env/lane_change.py
@@ -100,3 +100,59 @@ class LaneChangeTaskSpec(HighwayEnvTaskSpec):
         cot_prompts[cot_index] = cot_prompt
 
     return cot_prompts
+
+class LaneChangeTaskSpecCollision(LaneChangeTaskSpec):
+  def __init__(self, observations: np.ndarray, actions: np.ndarray, collision_observations: np.ndarray, collision_actions: np.ndarray, collision_indices: np.ndarray, cfgs: dict):
+    super().__init__(observations, actions, cfgs)
+    
+    assert 'action_sample_mode' in cfgs, "action_sample_mode must be specified"
+
+    self.collision_observations = collision_observations
+    self.collision_actions = collision_actions
+    self.collision_tidx  = collision_indices
+
+    self.hop_lane_ids, self.hop_indices = self._get_task_lane_ids()
+    self.cot_tidx, self.cot_observations, self.cot_collision_actions = self._get_collision_data()
+  
+  def _get_collision_data(self) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    # tidx is the index of the self.observations and self.actions
+    # cidx is the index of the self.collision_observations and self.collision_actions
+    
+    select_tidx = []
+    select_cot_observations = []
+    select_cot_collision_actions = []
+
+    all_unique_tidx = np.unique(self.collision_tidx)
+
+    action_sample_mode = self.cfgs['action_sample_mode']
+    
+    for tidx in all_unique_tidx:
+      coll_cidx_mask = self.collision_tidx == tidx
+      coll_cidxs = np.where(coll_cidx_mask)[0]
+      
+      if action_sample_mode == 'random':
+        cot_cidx = np.random.choice(coll_cidxs, 1, replace=False)
+      
+      elif action_sample_mode == 'future':
+        # choose the action that is the closest to the next gt action in the future
+        cot_cidx = -1
+        best_tidx_dist = np.inf
+
+        for cidx in coll_cidxs:
+          cot_action = self.collision_actions[cidx]
+          future_gt_actions = self.actions[tidx+1:].tolist()
+          if cot_action not in future_gt_actions:
+            continue
+          gt_action_dist = future_gt_actions.index(cot_action)
+          if gt_action_dist < best_tidx_dist:
+            best_tidx_dist = gt_action_dist
+            cot_cidx = cidx
+
+        if cot_cidx == -1:
+          cot_cidx = np.random.choice(coll_cidxs, 1, replace=False)
+
+      select_tidx.append(tidx)
+      select_cot_observations.append(self.collision_observations[cot_cidx])
+      select_cot_collision_actions.append(self.collision_actions[cot_cidx].astype(np.int32))
+
+    return select_tidx, select_cot_observations, select_cot_collision_actions
